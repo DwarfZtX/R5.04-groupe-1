efd039cbed3c6f2608eadc2934dafa74cb5c23b5
Hugo <Hugo.Joubat.Etu@univ-lemans.fr>
2026-09-14 14:59:26 +0200

    Signed-off-by: Hugo <Hugo.Joubat.Etu@univ-lemans.fr>

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
 
 $routes = [
-    '/' => 'accueil',
-    '/about' => 'about',
-    '/interviews' => 'interviews',
-    '/podcasts' => 'podcasts'
+    '/~i2400919/WebdocAGENCE1/' => 'accueil',
+    '/~i2400919/WebdocAGENCE1/test' => 'test',
+    '/~i2400919/WebdocAGENCE1/about' => 'about',
+    '/~i2400919/WebdocAGENCE1/interviews' => 'interviews',
+    '/~i2400919/WebdocAGENCE1/podcasts' => 'podcasts'
 ];
 
 
@@ -14,6 +15,7 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (array_key_exists($path, $routes)) {
     // If it does, include the corresponding file
     echo $path;
+    include("about.html");
 } else {
     // If it doesn't, return a 404 error
     header("HTTP/1.1 404 Not Found");
